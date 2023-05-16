14c08545cdbbaa5e0b2216cfd9c57ff51b31c592
Robertm106 <131869211+Robertm106@users.noreply.github.com>
2023-05-16 17:59:51 +0000

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+# ask the user their name ?
+name = input("whats your name? ")
+
+
+# Remove whitespace from str
+name = name.strip()
+
+
+# Capitalize user's name
+name = name.capitalize()
+
+# Say hello to user
+print(F"hello, {name}")
